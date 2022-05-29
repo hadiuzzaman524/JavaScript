cf6d6db5e9eb9e52b972f19e906b0bc30ef6261a
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2022-05-29 17:35:03 +0600

    learn about comment and variable in js

---
+++ index.js
@@ -0,0 +1,24 @@
+// This is single line comment 
+
+/*
+This is multi line comment
+*/
+
+// Variable 
+
+var x= 'hadi'; 
+var y= "jaman"; 
+
+console.log(x+y);
+
+let m=42; 
+let n= 42.442; 
+
+console.log(m+n); 
+
+let z; 
+console.log(typeof z);
+
+let flag=true; 
+
+console.log(typeof flag); 
