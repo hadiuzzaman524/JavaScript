d1fae67eec546bd5e0ce3b768016bd0a3c224a7e
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2022-05-29 17:41:48 +0600

    learn about operator on js

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ This is multi line comment
 
 // Variable 
 
+/*
+There are 4 type of veriable in js
+1. number
+2. string 
+3. boolean
+4. undefined
+*/
 var x= 'hadi'; 
 var y= "jaman"; 
 
@@ -13,12 +20,27 @@ console.log(x+y);
 
 let m=42; 
 let n= 42.442; 
-
 console.log(m+n); 
-
 let z; 
 console.log(typeof z);
-
 let flag=true; 
-
 console.log(typeof flag); 
+
+// Operator 
+/*
+Like other programming language js have 
+1. conditional
+2. assignment
+3. logical
+4. arithmetic etc
+
+There are 2 special operator in js
+1. (condition) ? true: false // conditional operator
+2. typeof var
+*/
+
+let num1=100; 
+let num2=200; 
+let big= num1>num2? num1: num2; 
+console.log("Big number is: "+big);
+console.log("Type of big: ", typeof big);
